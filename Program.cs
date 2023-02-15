08f091ca8afa4b9af8b7038c0073effb091ae241
“codewitherrors” <codewitherrors1@gmail.com>
2023-02-15 14:49:50 +0530

    Add : Program.cs File

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Line Comparision Problem Using OOPs");
+            FanofGeometry f1 = new FanofGeometry();
+            f1.fanOfGeometry();
         }
     }
 }
